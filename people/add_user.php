45c0c23599b4c336a31769519fa8d5376261f00b
Arunpragash Annanperiasamy <arunpragash46@gmail.com>
2023-09-22 14:42:04 +0530

    People General pages are added with form data

---
+++ people/add_user.php
@@ -0,0 +1,82 @@
+<div class="page-wrapper">
+    <div class="content">
+        <div class="page-header">
+            <div class="page-title">
+                <h4>User Management</h4>
+                <h6>Add/Update User</h6>
+            </div>
+        </div>
+        <form enctype="multipart/form-data" action="/people/add_user" id="new_form" method="post">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>First Name</label>
+                                <input type="text" name="first_name" id="first_name">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Last Name</label>
+                                <input type="text" name="last_name" id="last_name">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>User Name</label>
+                                <input type="text" name="user_name" id="user_name">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                        <div class="form-group">
+                            <label>Password</label>
+                            <div class="pass-group">
+                                <input type="password" class=" pass-input" name="password" id="password">
+                                <span class="fas toggle-password fa-eye-slash"></span>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Phone</label>
+                                <input type="text" name="mobile_number" id="mobile_number">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="text" name="email_id" id="email_id">
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6 col-12">
+                            <div class="form-group">
+                                <label>Role</label>
+                                <select class="select form-select" name="role" id="role">
+                                    <option>Select</option>
+                                    <option>Owner</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <label> User Image</label>
+                                <div class="image-upload">
+                                    <input type="file" name="user_image" id="user_image">
+                                    <div class="image-uploads">
+                                        <img src="/assets/img/icons/upload.svg" alt="img">
+                                        <h4>Drag and drop a file to upload</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <a href="javascript:void(0);" class="btn btn-submit me-2">Submit</a>
+                            <a href="userlist.html" class="btn btn-cancel">Cancel</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
