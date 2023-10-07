9cde0995700ab430664e283ec875d746b7af92a0
Arunpragash Annanperiasamy <arunpragash46@gmail.com>
2023-10-07 19:40:08 +0530

    Changed the comment

--- functions/functions.js
+++ functions/functions.js
@@ -154,14 +154,10 @@ $(document).ready(function () {
   $("a.page_url").click(function (event) {
     event.preventDefault(); // Prevent the default link behavior (page reload)
 
-    // Get the target URL from the href attribute
-    var targetUrl = $(this).attr("href");
-    console.log("target " + targetUrl);
-    // Change the URL without a full page reload
-    window.history.pushState({}, "", targetUrl);
+    
+    var targetUrl = $(this).attr("href"); // Get the target URL from the href attribute
+    window.history.pushState({}, "", targetUrl); // Change the URL without a full page reload
 
-    // You can also update the page content here if needed
-    // Optionally, trigger any other actions related to the link click
     change_content();
     $(this).closest(".submenu").children("a").addClass("active subdrop");
     return false; // Prevent any other click handlers from executing
