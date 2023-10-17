57c2d2ff60e76021dc44fd0a488ead5e206a18cc
Arunpragash Annanperiasamy <arunpragash46@gmail.com>
2023-10-17 10:26:13 +0530

    Revert "Temporarly Handling the request for insert, update, delete and so on"
    This reverts commit 1d1f0793564c263be4130ec782c3539e1c7fc2c3.

--- functions/form_submit.php
+++ functions/form_submit.php
@@ -5,17 +5,21 @@ $headers = getallheaders();
 $data = json_decode(file_get_contents('php://input'), true); // For JSON requests
 
     if($method === "POST"){
-        echo "POST method";
-        return;
+        // header('Content-Type: application/json');
+        http_response_code(200);
+        echo "data received";
+        // if(strtolower($_POST['method']) === 'insert'){
+        //     return insert($_POST['form'], $_POST['page']);
+        // }
+        
     }
-    // if($_SERVER["REQUEST_METHOD"] === "GET"){
-    //     echo "Post request";
-    // }
-    // if($_SERVER["REQUEST_METHOD"] === "PUT"){
-    //     echo "Post request";
-    // }
-    // if($_SERVER["REQUEST_METHOD"] === "DELETE"){
-    //     echo "Post request";
-    // }
-    
+    if($_SERVER["REQUEST_METHOD"] === "GET"){
+        echo "Post request";
+    }
+    if($_SERVER["REQUEST_METHOD"] === "PUT"){
+        echo "Post request";
+    }
+    if($_SERVER["REQUEST_METHOD"] === "DELETE"){
+        echo "Post request";
+    }
 ?>
